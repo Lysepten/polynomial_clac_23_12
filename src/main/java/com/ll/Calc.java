a666c23091fd8fa12885adb6a3e921440a13b338
Lysepten <tlstlsrbrb11@gamil.com>
2023-12-27 09:47:03 +0900

    Helë¹10 + 20 + 30

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,36 +2,32 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) {
-//    String[] bits;
-//    contains()
 
     boolean needToPlus = exp.contains("+");
     boolean needToMinus = exp.contains("-");
 
-    if (exp.equals("123 + 456")) {
-      String[] bits = exp.split(" \\+ ");
-      int a = Integer.parseInt(bits[0]);
-      int b = Integer.parseInt(bits[1]);
-      return a + b;
-    } else if (exp.contains("+" + "+")) {
-      String[] bits = exp.split(" \\+ ");
-      int a = Integer.parseInt(bits[0]);
-      int b = Integer.parseInt(bits[1]);
-      int c = Integer.parseInt(bits[2]);
-      return a + b + c;
-    } else if (exp.contains("-" + "-")) {
-      String[] bits = exp.split(" - ");
-      int a = Integer.parseInt(bits[0]);
-      int b = Integer.parseInt(bits[1]);
-      int c = Integer.parseInt(bits[2]);
-      return a - b - c;
-    }  else if (exp.contains("-")) {
-      String[] bits = exp.split(" - ");
-      int a = Integer.parseInt(bits[0]);
-      int b = Integer.parseInt(bits[1]);
-      return a - b;
+    String[] bits = null;
+
+    if (needToPlus) {
+      bits = exp.split(" \\+ ");
+    } else if (needToMinus) {
+      bits = exp.split(" - ");
     }
 
-    throw new RuntimeException("해석할 수 없음");
+    int a = Integer.parseInt(bits[0]);
+    int b = Integer.parseInt(bits[1]);
+    int c = 0;
+
+    if (bits.length > 2) {
+      c = Integer.parseInt(bits[2]);
+    }
+
+    if (needToPlus) {
+      return a + b + c;
+    } else if (needToMinus) {
+      return a - b - c;
+    }
+
+    throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
 }
