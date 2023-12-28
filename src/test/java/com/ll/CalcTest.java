7f1da4dfd5fdab469a411bbc42706259419b6c66
Lysepten <tlstlsrbrb11@gamil.com>
2023-12-28 10:02:26 +0900

    (10 + 20) * 3

--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -114,4 +114,34 @@ public class CalcTest {
     assertThat(Calc.run("(((10 + 20)))")).isEqualTo(30);
   }
 
+  @Test
+  @DisplayName("100 == 100")
+  void t19() {
+    assertThat(Calc.run("100")).isEqualTo(100);
+  }
+
+  @Test
+  @DisplayName("(100) == 100")
+  void t20() {
+    assertThat(Calc.run("(100)")).isEqualTo(100);
+  }
+
+  @Test
+  @DisplayName("(100)  == 100")
+  void t21() {
+    assertThat(Calc.run("(100) ")).isEqualTo(100);
+  }
+
+  @Test
+  @DisplayName("(20 + 20) + 20 == 60")
+  void t22() {
+    assertThat(Calc.run("(20 + 20) + 20")).isEqualTo(60);
+  }
+
+  @Test
+  @DisplayName("(10 + 20) * 3 == 90")
+  void t23() {
+    assertThat(Calc.run("(10 + 20) * 3")).isEqualTo(90);
+  }
+
 }
